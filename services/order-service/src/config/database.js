a7517c54445430ff78ca28b7a23311194fd6cf08
VM Dane <vm_dane@MacBook-Air-de-VMD.local>
2025-06-05 12:13:28 +0200

    Suite des modifications

--- services/order-service/src/config/database.js
+++ services/order-service/src/config/database.js
@@ -8,7 +8,7 @@ export const connectDB = async () => {
       return;
     }
 
-    const conn = await mongoose.connect(process.env.MONGODB_URI_ORDER);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
